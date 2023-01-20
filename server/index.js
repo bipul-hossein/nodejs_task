1473715a41b93918833c8dd3c8ee552e1f2611c6
Bipul-hossen <mdbipulhossain252@gmail.com>
2023-01-20 18:02:24 +0600

    connection to database

--- server/index.js
+++ server/index.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 const app = express();
 const port = 5000;
 
 app.use(cors())
 
+
+
+const uri = `mongodb+srv://nodejs_task:N2M2sd4TQzo03bvK@cluster0.jvivwjj.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const UserCollection = client.db("nodejs_task").collection("students_details");
+
+        app.get('/students', async (req, res) => {
+            const query = {}
+            const cursor = UserCollection.find(query);
+            const students = await cursor.toArray();
+            res.send(students);
+        })
+    } finally { }
+}
+run().catch(err => console.log(err));
+
 app.get('/', (req, res) => {
-res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
-console.log('Example app listening on port', port);
+    console.log('Example app listening on port', port);
 });
